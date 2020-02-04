4ba283459873c3947cc4d38a96c5b7629e70a713
Mohaned Hassabo <mohaned.hassabo@hotmail.com>
2020-02-04 16:56:19 +0400

    feature/task1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,7 +15,11 @@ export class HomeComponent implements OnInit {
     //answers: Movie[];
 
     isDisplayed = false;
-    
+    //Questions Variables
+    questionOne: "";
+
+    //Answers Variables
+    answerOne: [];
 
     constructor(@Inject(AppConfig) public appConfig: any,
                 private movieService: MovieService
@@ -30,6 +34,21 @@ export class HomeComponent implements OnInit {
         }
     }
     showAllTasks() {
+        let task1 = this.movieService.getMoviesInfo('GetLongestOpeningCrawlMovie');
+
+        //Combine all requests and get combined results at the same time - assign the returned results into a variables
+        forkJoin([task1])
+        //.map((res: Response) => res.json().response.map((data: Movie) => new Movie().deserialize(data)))
+            .subscribe(([res1]) => {
+                var me = this;
+                //Get all results for each task/question and assign them to variables
+                me.questionOne= res1['question'];
+                me.answerOne= res1['answer'];
+            }, (error) => {
+                console.log('Error found!');
+            }, () => {
+                console.log('Request Complete!');
+            });
 
     }
 }
