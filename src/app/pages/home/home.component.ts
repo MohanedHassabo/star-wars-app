688764f53099cd32b37492a7ec25cc623d5becc1
Mohaned Hassabo <mohaned.hassabo@hotmail.com>
2020-02-04 17:17:35 +0400

    Final combined results

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,7 +15,18 @@ export class HomeComponent implements OnInit {
     //answers: Movie[];
 
     isDisplayed = false;
-    
+
+    //Questions Variables
+    questionOne: "";
+    questionTwo: "";
+    questionThree: "";
+    questionFour: "";
+
+    //Answers Variables
+    answerOne: [];
+    answerTwo: [];
+    answerThree: [];
+    answerFour: [];
 
     constructor(@Inject(AppConfig) public appConfig: any,
                 private movieService: MovieService
@@ -30,6 +41,35 @@ export class HomeComponent implements OnInit {
         }
     }
     showAllTasks() {
+        //get request call for each task from `movieService` and assign result to a variable
+        let task1 = this.movieService.getMoviesInfo('GetLongestOpeningCrawlMovie');
+        let task2 = this.movieService.getMoviesInfo('GetPersonAppearedInMostFilms');
+        let task3 = this.movieService.getMoviesInfo('GetSpeciesApearedInMostFilms');
+        let task4 = this.movieService.getMoviesInfo('GetPlanetWithMoreVehiclePilots');
+
+        /*this.movieService.getMovies('GetLongestOpeningCrawlMovie').subscribe(answers => this.answers = answers);
+        console.log(this.answers);*/
+
+        //Combine all requests and get combined results at the same time - assign the returned results into a variables
+        forkJoin([task1, task2, task3, task4])
+            //.map((res: Response) => res.json().response.map((data: Movie) => new Movie().deserialize(data)))
+            .subscribe(([res1, res2, res3, res4]) => {
+                var me = this;
+                //Get all results for each task/question and assign them to variables
+                me.questionOne= res1['question'];
+                me.questionTwo = res2['question'];
+                me.questionThree = res3['question'];
+                me.questionFour = res4['question'];
+
+                me.answerOne= res1['answer'];
+                me.answerTwo = res2['answer'];
+                me.answerThree = res3['answer'];
+                me.answerFour = res4['answer'];
+            }, (error) => {
+                console.log('Error found!');
+            }, () => {
+                console.log('Request Complete!');
+            });
 
     }
 }
