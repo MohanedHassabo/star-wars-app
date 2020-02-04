88000331234e97eea0d9633c681177a3a15cb561
Mohaned Hassabo <mohaned.hassabo@hotmail.com>
2020-02-04 16:58:16 +0400

    feature/task2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,7 +15,11 @@ export class HomeComponent implements OnInit {
     //answers: Movie[];
 
     isDisplayed = false;
-    
+    //Questions Variables
+    questionTwo: "";
+
+    //Answers Variables
+    answerTwo: [];
 
     constructor(@Inject(AppConfig) public appConfig: any,
                 private movieService: MovieService
@@ -30,6 +34,21 @@ export class HomeComponent implements OnInit {
         }
     }
     showAllTasks() {
+        let task2 = this.movieService.getMoviesInfo('GetPersonAppearedInMostFilms');
+
+        //Combine all requests and get combined results at the same time - assign the returned results into a variables
+        forkJoin([task2])
+        //.map((res: Response) => res.json().response.map((data: Movie) => new Movie().deserialize(data)))
+            .subscribe(([res2]) => {
+                var me = this;
+                //Get all results for each task/question and assign them to variables
+                me.questionTwo = res2['question'];
+                me.answerTwo = res2['answer'];
+            }, (error) => {
+                console.log('Error found!');
+            }, () => {
+                console.log('Request Complete!');
+            });
 
     }
 }
